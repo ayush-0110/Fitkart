47ca6e515499ffee13841d1056d571b9e580fdcc
Ayush <200104019.hbtu.ac.in>
2022-10-09 22:13:17 +0530

    first commit

---
+++ aa.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#define ll long long
+using namespace std;
+int main()
+{
+    ll a=69;
+    do
+    {
+    a-=70;
+    
+    cout<<a<<" DUH!!\n";
+    cout<<a<<" DUH!! \n";
+    cout<<a<<" DUH!! ";
+    cout<<a<<" DUH!!\nhiii \n";
+    } while (a>0);
+}
